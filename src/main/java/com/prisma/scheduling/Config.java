399e8a124e445e441386e2098e272097866e08f2
isidrovasquez <armandolo21@hotmail.com>
2020-04-23 16:07:11 -0400

    Config Added

---
+++ src/main/java/com/prisma/scheduling/Config.java
@@ -0,0 +1,9 @@
+package com.prisma.scheduling;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = {"com.prisma"})
+@Configuration
+public class Config {
+
+}
