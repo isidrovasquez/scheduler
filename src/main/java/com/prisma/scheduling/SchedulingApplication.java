802db2369def8bdbe6cc1f3f17a1947f7e8a1f0b
isidrovasquez <armandolo21@hotmail.com>
2020-04-23 16:37:24 -0400

    @ComponentScan moved

--- src/main/java/com/prisma/scheduling/SchedulingApplication.java
+++ src/main/java/com/prisma/scheduling/SchedulingApplication.java
@@ -2,9 +2,11 @@ package com.prisma.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.prisma"})
 @EnableScheduling
 public class SchedulingApplication {
 
